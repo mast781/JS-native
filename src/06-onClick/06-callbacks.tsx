982a224756610758ebea7f0fc2b0bbd8a8c9f5b4
Evgenyj <worker_lab@mail.ru>
2022-01-02 19:32:01 +0300

    Js-onClick-final

--- src/06-onClick/06-callbacks.tsx
+++ src/06-onClick/06-callbacks.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {ChangeEvent, MouseEvent} from "react";
+
 
 /*const callback = () => {
     alert("hey")
@@ -7,16 +8,31 @@ window.setTimeout(callback, 1000)*/
 
 export const User = () => {
 
-    const deleteUser = () => {
-        alert("user have be deleted")
+    const search = (event: MouseEvent<HTMLButtonElement>) => {
+
+        alert(event.currentTarget.name)
     }
 
     const saveUser = () => {
         alert("user shave be saved")
     }
 
-    return <div>Dimych
-        <button onClick={deleteUser}>delete</button>
-        <button onClick={saveUser}>save</button>
+    const onNameChanged = () => {
+        console.log("name changed")
+    }
+
+    const onAgeChanged = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log("age changed: " + event.currentTarget.value)
+    }
+
+    const focusLostHandler = () => {
+        console.log("focus lost")
+    }
+
+    return <div>
+        <textarea onChange={onNameChanged} onBlur={focusLostHandler}>Dimych</textarea>
+        <input onChange={onAgeChanged} type={"number"}/>
+
+        <button name="delete" onClick={search}>search</button>
     </div>
 }
