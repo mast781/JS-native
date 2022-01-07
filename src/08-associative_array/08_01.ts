10828fa329c71df18ae9658d88585dda1718204a
Evgenyj <worker_lab@mail.ru>
2022-01-07 16:08:40 +0300

    Js-associativeArray-progress

--- src/08-associative_array/08_01.ts
+++ src/08-associative_array/08_01.ts
@@ -1,6 +1,3 @@
-export const usersArray = ['Dimych', 'Natasha', 'Valera', 'Katya']
-
-
 export const usersObj = {
     '0': 'Dimych',
     '1': 'Natasha',
@@ -8,9 +5,31 @@ export const usersObj = {
     '3': 'Katya'
 }
 
-export const users = {
-    '0': {id: 101, name: 'Dimych'},
-    '1': {id: 3232312, name: 'Natasha'},
-    '2': {id: 1212, name: 'Valera'},
-    '3': {id: 1, name: 'Katya'}
+export type UsersType = {
+    [key: string]: {id: number, name: string}
 }
+
+export const users: UsersType = {
+    '101': {id: 101, name: 'Dimych'},
+    '3232312': {id: 3232312, name: 'Natasha'},
+    '1212': {id: 1212, name: 'Valera'},
+    '1': {id: 1, name: 'Katya'}
+}
+
+// users[1]
+
+let user = {id: 100500, name: 'Igor'}
+users[user.id] = user // добавление
+delete users[user.id] // удаление
+
+
+export const usersArray = [
+    {id: 101, name: 'Dimych'},
+    {id: 3232312, name: 'Natasha'},
+    {id: 1212, name: 'Valera'},
+    {id: 1, name: 'Katya'}
+]
+
+//usersArray.find(u => u.id === 1)
+//var usersCopy = [...usersArray.filter(),user] //добавление в массив
+//var usersArray = userArray.filter(u => u.id !== user.id) //удаление из массива
