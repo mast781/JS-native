20b5b9fb208bc8d1ed4ea0942971a344d9b2bd6a
Evgenyj <worker_lab@mail.ru>
2022-01-07 13:21:07 +0300

    Js-destructuring-progress-4

--- src/07-destructuring/07-destructuring.test.ts
+++ src/07-destructuring/07-destructuring.test.ts
@@ -5,7 +5,7 @@ beforeEach(() => {
     props = {
         name: "Dimych",
         age: 32,
-        lessons:[{title: "1"}, {title: "2"}],
+        lessons:[{title: "1"}, {title: "2"}, {title: "3"}],
         address: {
             street: {
                 title: "Nezavisimosti street"
@@ -31,13 +31,27 @@ test("1", () => {
 
 
     expect(age).toBe(32)
-    expect(lessons.length).toBe(2)
+    expect(lessons.length).toBe(3)
 
     expect(a).toBe(32)
-    expect(l.length).toBe(2)
+    expect(l.length).toBe(3)
     expect(title).toBe("Nezavisimosti street")
 })
 
 test("2", () => {
+    const l1 = props.lessons[0]
+    const l2 = props.lessons[1]
 
+    // const [ls1, ls2] = props.lessons // аналогично записи выше, но количество переменных должно соответствовать количеству элементов массива
+    // const [,ls2] = props.lessons // если нужен не первый элемент, то оставляем запятую
+    // const [ls1, ...restLessons] = props.lessons // берется первый элемент, остальные пихаются в массив restLessons
+    const [, ls2, ...restLessons] = props.lessons // первый пропускается, берется второй элемент, третий пихается в массив restLessons
+    expect(l1.title).toBe("1")
+    expect(l2.title).toBe("2")
+
+    // expect(ls1.title).toBe("1")
+    expect(ls2.title).toBe("2")
+
+    expect(restLessons.length).toBe(1)
+    expect(restLessons[0].title).toBe("3")
 })
--- src/07-destructuring/destructuring.tsx
+++ src/07-destructuring/destructuring.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 type LessonType = {
     title: string
@@ -23,15 +23,27 @@ type PropsType = {
     car: { model: string }
 }
 
+function useDimychState(m: string) {
+    return [m, function(){}]
+}
+
+function useDimychState2(m: string) {
+    return {
+        message:m,
+        setMessage: function(){}}
+}
+
 // export const ManComponent: React.FC<PropsType> = (props) => { // можно сделать деструктуризацию в параметрах функции напрямую
 
 // export const ManComponent: React.FC<PropsType> = ({title, man, car,food}) => {
-export const ManComponent: React.FC<PropsType> = ({title, man, ...props}) => { // это означает создать переменные для title и man, а остальные запихать в props
+export const ManComponent: React.FC<PropsType> = ({title, man, ...props}) => { // это означает создать переменные для title и man, а остальные запихать в props, можно вынести это отдельно в const
     // const {title} = props
     // const {name} = props.man
     /// const {title, man: {name}} = props // аналогично записи выше, но лучше исплользовать отдельно
     // const {title, man} = props // можно передать в переменную объект и образаться к немуб как показано ниже в div
 
+    // const [message, setMessage] = useState<string>("hello")
+    const [message, setMessage] = useDimychState("hello")
 
     return <div>
         <h1>{title}</h1>
